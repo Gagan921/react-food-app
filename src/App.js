bed43063c9de3b0f76aafceb747f42fd4bd4c1ee
Gagan921 <63442672+Gagan921@users.noreply.github.com>
2022-01-11 09:55:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+//eslint-disable-next-line
+import Helloworld from './component/Basics/Helloworld'
+
+const App = () => {
+
+    return <Helloworld />;       
+}
+
+export default App;
